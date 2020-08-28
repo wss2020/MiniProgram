732a79838d86e4a249e5058b621f43852ad8b2c0
GeekUniverity <wangshuaishuai@yidiansongli.com>
2020-08-28 15:45:42 +0800

    pages.js注释

--- 以前的cs/service/pages.js
+++ 以前的cs/service/pages.js
@@ -154,7 +154,7 @@ let mpages = {
                         let func = _events[key];
                         if (typeof func == 'function') {
                             func(data);
-                            console.log('执行了funch函数，函数名为')
+                            console.log('执行了func函数，函数名为');
                             console.log(func);
                         }
                     }
@@ -236,3 +236,113 @@ let mpages = {
 };
 
 module.exports = [mpages];
+
+
+/***
+ *
+ 页面传参（不刷新页面，思路）
+ 1.传入数据，到下一页面
+ 2.从下一页面，返回数据到上一页面。
+ 准备工作：
+ 引入了 event、mixins  、promise
+ 基本思路，利用页面栈 、广播 、promise 的回调函数  、把函数作为参数传入并在指定时间执行（回调）
+ 思路：
+ 1.传入数据到下一页面，
+ 页面执行，
+ //页面传参数
+ param(){
+    this.navigateModal("/pages/index/param/param", {
+        amount: 13
+    }).then((res) => {
+        console.log('接收resolve,返回的值res为');
+        console.log(res);
+        return app.func.toastPromise("return:" + res);
+    }).catch((res) => {
+        console.log(res);
+    })
+}
+ 预先写好，promise 变为 resolve 状态下所执行的操作，也就是接收下一个页面传入的数据
+ mixins，事先对，navigateModal 进行了处理，
+ navigateModal 接收两个参数，第一个参数，页面跳转路径，第二个参数，传入下一页面的数据。
+ 返回一个 promise对象 ;
+ .then(res)   是当promise对象变为 resolve 状态执行，res 为下一个页面传入的数据。
+ promise对象中，执行默认的promise,函数中执行  this.navigateTo.
+
+ navigateTo   接收两个参数，
+ 第一个参数url, 页面跳转路径，
+ 第二个参数 _event 是一个对象，有两个属性，onBack、onRet , 两个回调函数，分别对应 reject() 、resolve();
+ 返回一个promise对象。
+ .then(res)  执行的是一个 res.event.emit(‘args’,agrs);     agrs 是 navigateModal 函数的第二个参数，就是页面传入的数据
+ promise对象中，执行的是，创建一个 EventEmitter 对象赋值给 this._outEvent ,创建监听 this._outEvent.on(‘ready’,(e)={}); 指定回调函数，
+ 回调函数中，分为两部分，一个是执行 resolve
+ let childEvent = e;
+ resolve({                                 // 指定成功的回调    childEvent 是二级页面创建的 eventEmitter 对象。
+    event: new _EventChannel(childEvent, this._outEvent)    // 把 ready 监听器的回调函数的参数，  再做一次处理。
+});
+ 这一部分和接收上一个页面的传入的参数有关，resolve()正好对应，执行，
+ 接着执行了，二级页面，onLoad 中 args 的监听的回调函数。
+ onLoad(options) {
+   // channel 当前页面创建的 _EventChannel()类，返回的对象
+    let channel = this.getEventChannel();
+    channel.on('args', (args) => {
+        if (this.onModal != null) {
+            this.onModal(args);
+        }
+    });
+},
+ 这时，传入数据结束。
+
+ 另一个是执行，监听
+ childEvent.on('user', (e) => {
+    let key = e.key;
+    let data = e.data;
+    if (_events != null && _events.hasOwnProperty(key)) {
+        let func = _events[key];
+        if (typeof func == 'function') {
+            func(data);
+        }
+    }
+});
+ 内部回调函数，根据 传入的key 和 navigateTo 的第二个对象参数 判断执行， 第二个对象参数中的那个回调，触发 resolve(), 还是 reject( )，
+ 触发 resolve( data), 正好对应执行，
+
+ closeModal(ret) {
+    if (ret == null) {
+        ret = true;
+    }
+    this.data._ret = ret;
+    wx.navigateBack();
+},
+ // 生命周期函数--监听页面卸载
+onUnload() {
+    if (this.data._ret == null) {
+        this.emitOpener('onBack')
+    } else {
+        this.emitOpener('onRet', this.data._ret);
+    }
+},
+emitOpener: function (eventName, args) {
+    let channel = this.getEventChannel();              // 获取当前页面创建的广播类，并把它赋值给 channel
+    if (channel != null && channel.emit != null) {
+        channel.emit("user", {key: eventName, data: args});
+    }
+},
+
+如上，用户主动触发closeModal 函数，并且传入 需要传给上一个页面的数据，并调用 wx.navigateBack(),返回上一页面。
+此时触发，页面生命周期 onUnload,  根据用户，有没有传给上一个页面数据，来判断，user 的广播，传入的数据 key 的值。
+然后触发  user 的监听。
+
+最后，关于页面栈哪一块，自己看代码。
+
+
+*/
+
+
+
+
+
+
+
+
+
+
