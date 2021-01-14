a80fc9bcb97cee67e77cac2f57a614ea3a7332e6
王帅 <wangshuaishuai@yidiansongli.com>
2021-01-14 17:10:01 +0800

    time

---
+++ 以前的cs/pages/index/layout/layout.js
@@ -0,0 +1,71 @@
+// pages/index/layout/layout.js
+Page({
+
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		arrImgs:[
+			{src:'https://images.shobserver.com/news/690_390/2021/1/14/d9646738e80a454daecac2272145cb68.jpg'},
+			{src:'https://images.shobserver.com/news/690_390/2021/1/14/d9646738e80a454daecac2272145cb68.jpg'},
+			{src:'https://images.shobserver.com/news/690_390/2021/1/14/d9646738e80a454daecac2272145cb68.jpg'},
+			{src:'https://images.shobserver.com/news/690_390/2021/1/14/d9646738e80a454daecac2272145cb68.jpg'},
+		]
+	},
+
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {
+
+	},
+
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {
+
+	},
+
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {
+
+	},
+
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {
+
+	}
+})
