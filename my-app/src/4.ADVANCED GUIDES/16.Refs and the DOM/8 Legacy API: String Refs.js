e52f84ce02a1f86bac9369fdc0dee9c340fe8eb6
王帅 <wangshuaishuai@yidiansongli.com>
2021-02-02 10:51:24 +0800

    ref

---
+++ my-app/src/4.ADVANCED GUIDES/16.Refs and the DOM/8 Legacy API: String Refs.js
@@ -0,0 +1,10 @@
+/**
+ 过时 API：String 类型的 Refs
+    如果你之前使用过 React，你可能了解过之前的 API 中的 string 类型的 ref 属性，例如 "textInput"。你可以通过 this.refs.textInput 来
+ 访问 DOM 节点。我们不建议使用它，因为 string 类型的 refs 存在 一些问题。它已过时并可能会在未来的版本被移除。
+
+
+
+ 注意
+ 如果你目前还在使用 this.refs.textInput 这种方式访问 refs ，我们建议用回调函数或 createRef API 的方式代替
+ */
